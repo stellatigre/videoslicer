7e179698542dae2caffed0f621f621f0dff647a6
Stella Tigre <stella.cannefax@gmail.com>
2015-12-22 23:01:03 -0600

    limit ffmpeg threads to number of cpu cores, general re-org

--- slicer.js
+++ slicer.js
@@ -1,5 +1,7 @@
+var os = require('os');
 var fs = require('fs');
 var path = require('path');
+var async = require('async');
 var cli = require('commander');
 var ffmpeg = require('fluent-ffmpeg');
 
@@ -8,11 +10,15 @@ function safeMkdirSync(dir) {
     catch (e) { fs.mkdirSync(dir) }
 }
 
-function saveSlice(file, start, length, outDir) {
+function saveSlice(file, start, length, outDir, callback) {
     var extension = path.extname(file);
     var end = start + length;
     ffmpeg(file).seekInput(start).duration(length)
-        .save(path.join(outDir, `${start}_to_${end}${extension}`));
+        .save(path.join(outDir, `${start}_to_${end}${extension}`))
+        .on('end', ()=> {
+            console.log(`saved slice ${start}_to_${end}`);
+            callback();
+        });
 }
 
 function slice(file, duration) {
@@ -22,10 +28,20 @@ function slice(file, duration) {
     ffmpeg.ffprobe(file, (err, data) => {
         var vidLength = data.streams[0].duration;
         var sliceCount = Math.floor(vidLength / duration);
-        console.log(`slicing ${path.basename(file)} into ${sliceCount} slices`);
-        for (var i=0; i < sliceCount; i++) {
-            saveSlice(file, i * duration, duration, outputDir);
-        }
+        renderSlices(file, duration, sliceCount, outputDir);
+    });
+}
+
+function renderSlices(file, duration, sliceCount, outputDir) {
+    console.log(`slicing ${path.basename(file)} into ${sliceCount} slices`);
+    var sliceRange = Array.apply(0, Array(sliceCount)).map((_, y) => y + 1);
+
+    // only spawn as many ffmpeg processes at a time as we have cpus
+    // otherwise it will render the computer unresponsive on larger files
+    async.eachLimit(sliceRange, os.cpus().length, (i, cb) => {
+        saveSlice(file, i * duration, duration, outputDir, cb);
+    }, () => {
+        console.log("done!");
     });
 }
 
